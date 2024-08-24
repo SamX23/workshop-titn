326a71a95e3aba0520092229672dc6b4d3e5f322
SamX23 <skalexsong@gmail.com>
2024-08-25 06:36:56 +0700

    chore: fix build error

--- src/app/city/[city]/page.tsx
+++ src/app/city/[city]/page.tsx
@@ -45,7 +45,7 @@ const CityPage = async ({ params }: { params: { city: string } }) => {
           >
             <path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" />
           </svg>
-          <Temperature temperature={data?.main.temp.toFixed(0) || "0"} />
+          <Temperature temperature={data?.main?.temp?.toFixed(0) || "0"} />
           <p className="text-xl text-gray-300">Partly Cloudy</p>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default async function Home() {
           >
             <path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" />
           </svg>
-          <Temperature temperature={data?.main.temp.toFixed(0) || "0"} />
+          <Temperature temperature={data?.main?.temp?.toFixed(0) || "0"} />
           <p className="text-xl text-gray-300">Partly Cloudy</p>
         </div>
       </div>
